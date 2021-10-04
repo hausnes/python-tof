a4e68a0ebe8e68d7e8cbddef8968f212fbf528e2
Jo Bjørnar Hausnes <jb.hausnes@gmail.com>
2021-10-04 10:50:30 +0200

    Tilbakemelding høg/låg, farge, ant. forsøk

--- guizeroEksempel1-gjettespel.py
+++ guizeroEksempel1-gjettespel.py
@@ -1,18 +1,49 @@
+# https://lawsie.github.io/guizero/textbox/
+
 from guizero import App, Text, PushButton, TextBox
 import random
 
 tilfeldigTal = random.randint(1,101)
+antallForsok = 0
+
+def riktigSvar():
+    app.bg = "green"
+    innlestTal.bg = "white"
+    startBeskjed.bg = "white"
+    button.bg = "white"
+
+def feilSvar():
+    app.bg = "red"
+    innlestTal.bg = "white"
+    startBeskjed.bg = "white"
+    button.bg = "white"
 
 def gjett():
     gjettaTal = int(innlestTal.value)
+    global antallForsok
+    antallForsok += 1
     if gjettaTal < tilfeldigTal:
         resultat.value = "Du gjetta for lågt."
+        feilSvar()
     elif gjettaTal > tilfeldigTal:
         resultat.value = "Du gjetta for høgt."
+        feilSvar()
     elif gjettaTal == tilfeldigTal:
         resultat.value = "Riktig!"
+        riktigSvar()
     else:
         resultat.value = "Du gjetta: " + str(gjettaTal) + ", men dette var feil."
+
+    resultat.value += "\nDu har brukt " + str(antallForsok) + " forsøk."
+
+'''
+def startPaaNytt():
+    global tilfeldigTal = random.randint(1,101)
+    innlestTal.bg = "white"
+    startBeskjed.bg = "white"
+    button.bg = "white"
+    resultat._text = ""
+'''
 
 # Set opp GUI-en (brukergrensesnittet)
 app = App(title="Gjettespel")
@@ -21,5 +52,11 @@ innlestTal = TextBox(app, width="fill")
 innlestTal.focus()
 button = PushButton(app, text="Gjett!", command=gjett)
 resultat = Text(app, text="")
+resultat.text_size = 34
+# button = PushButton(app, text="Start på ny!", command=startPaaNytt)
+
+innlestTal.bg = "white"
+startBeskjed.bg = "white"
+button.bg = "white"
 
 app.display()
