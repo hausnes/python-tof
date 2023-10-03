195f0582976ffd09d19ddd4abf003a7fec966b77
Jo Bjørnar Hausnes <jb.hausnes@gmail.com>
2023-10-03 20:52:39 +0200

    Update 07-visualisering.py

--- grunnleggande/07-visualisering.py
+++ grunnleggande/07-visualisering.py
@@ -1,30 +1,22 @@
-# import scipy
+# Importerer pandas for å jobbe med CSV-fila og matplotlib for å teikne grafar
 import pandas as pd
+import matplotlib.pyplot as plt
 
 # Les inn data
-# data = scipy.loadtxt('07-visualisering-data.csv', skiprows=1)
-datav2 = pd.read_csv('07-visualisering-data.csv', sep=',', header=0)
+data = pd.read_csv('07-visualisering-data.csv', sep=',', header=0)
 
 # Lagre data i variablar vha pandas
-tid = datav2['tidspunkt']
-temperatur = datav2['temperatur']
-luftfuktighet = datav2['luftfuktighet']
-lufttrykk = datav2['lufttrykk']
-partikkelkonsentrasjon = datav2['partikkelkonsentrasjon']
-
-# Lagre data i variablar
-# tid = data[:,0]
-# temperatur = data[:,1]
-# luftfuktighet = data[:,2]
-# lufttrykk = data[:,3]
-# partikkelkonsentrasjon = data[:,4]
+tid = data['tidspunkt']
+temperatur = data['temperatur']
+luftfuktighet = data['luftfuktighet']
+lufttrykk = data['lufttrykk']
+partikkelkonsentrasjon = data['partikkelkonsentrasjon']
 
 # Plot temperatur
-import matplotlib.pyplot as plt
 plt.plot(tid, partikkelkonsentrasjon)
 plt.xlabel('Tid')
 plt.ylabel('PM2.5')
-plt.title('Partikkelkonsentrasjon i lufta'))
+plt.title('Partikkelkonsentrasjon i lufta')
 # rotate labels on the x-axis
 plt.xticks(rotation=90)
 # save figure as png
