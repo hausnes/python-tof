106bb5e2b685454b97d04713d7b30c22c9218d87
Jo Bjørnar Hausnes <jb.hausnes@gmail.com>
2023-10-03 13:45:19 +0200

    Spesifisering av kva w gjer eller kan føre til

--- grunnleggande/06-skrive-til-fil.py
+++ grunnleggande/06-skrive-til-fil.py
@@ -2,7 +2,7 @@
 
 verListe = [24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
 
-with open("06-verdata.csv", "w") as fil:
+with open("06-verdata.csv", "w") as fil: # NB: w står for write, og skriver over gamle data = tømmer filen
     fil.write("temperatur\n")
     for temperatur in verListe:
         fil.write(f"{temperatur}\n")
