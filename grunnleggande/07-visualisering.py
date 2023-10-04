e57ddac5159b8953678c2a582672249f5cc9bc2a
Jo Bjørnar Hausnes <jb.hausnes@gmail.com>
2023-10-04 21:27:22 +0200

    Visualisering av data - update

--- grunnleggande/07-visualisering.py
+++ grunnleggande/07-visualisering.py
@@ -1,12 +1,14 @@
 # Importerer pandas for å jobbe med CSV-fila og matplotlib for å teikne grafar
-import pandas as pd
-import matplotlib.pyplot as plt
+import pandas as pd # Installer pandas ved å bruke kommandoen "pip install pandas" i terminalen
+import matplotlib.pyplot as plt # Installer matplotlib ved å bruke kommandoen "pip install matplotlib" i terminalen
 
 # Les inn data
-data = pd.read_csv('07-visualisering-data.csv', sep=',', header=0)
+data = pd.read_csv('07-visualisering-data.csv', sep=',', header=0) # header=0 betyr at første rad er kolonnenamn
+
+print(data.describe()) # Skriv ut statistikk om data
 
 # Lagre data i variablar vha pandas
-tid = data['tidspunkt']
+tid = data['tidspunkt'] 
 temperatur = data['temperatur']
 luftfuktighet = data['luftfuktighet']
 lufttrykk = data['lufttrykk']
@@ -17,8 +19,6 @@ plt.plot(tid, partikkelkonsentrasjon)
 plt.xlabel('Tid')
 plt.ylabel('PM2.5')
 plt.title('Partikkelkonsentrasjon i lufta')
-# rotate labels on the x-axis
-plt.xticks(rotation=90)
-# save figure as png
-plt.savefig('07-visualisering-plot.png')
+plt.xticks(rotation=90) # rotate labels on the x-axis
+plt.savefig('07-visualisering-plot.png') # save figure as png
 plt.show()
