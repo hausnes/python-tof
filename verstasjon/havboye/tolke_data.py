413a52fb2a85240f6322926049cd56037ec36a17
Jo Bjørnar Hausnes <jb.hausnes@gmail.com>
2024-05-16 00:00:39 +0200

    Meir om å tolke data vha Pandas

--- verstasjon/havboye/tolke_data.py
+++ verstasjon/havboye/tolke_data.py
@@ -82,3 +82,21 @@ temp_max = np.max(temp)
 temp_min = np.min(temp)
 print('Høyeste temperatur: ',temp_max)
 print('Laveste temperatur: ',temp_min)
+print()
+
+# Bruker pandas for å se på mellom annet standardavvik, gjennomsnitt og kvartiler
+df = pd.DataFrame(data)
+print(df.describe())
+print()
+
+# Bruker pandas for å se nærmere på termperturdata
+print("Gjennomsnittstemperatur: ", df['24.2'].mean())
+print("Makstemperatur: ", df['24.2'].max()) # Finner maksimumstemperatur
+print("Minimumstemperatur: ", df['24.2'].min()) # Finner minimumstemperatur
+print()
+print("Antall ganger ullike temperaturer forekommer: ")
+print(df['24.2'].value_counts()) # Teller antall ganger hver verdi forekommer
+print()
+print("Temperaturen som forekommer oftest: ", df['24.2'].value_counts().idxmax()) # Finner den mest forekommende verdien
+print("Standardavvik: ", df['24.2'].std()) # Finner standardavvik
+print("Median: ", df['24.2'].median()) # Finner median, altså midtverdien
